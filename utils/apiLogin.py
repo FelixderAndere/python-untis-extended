eb526572c70bd24dfba1cc4be36669119f2b4114
Felix <203254529+FelixderAndere@users.noreply.github.com>
2026-07-02 19:22:53 +0200

    refactoring apiLogin.py

--- utils/apiLogin.py
+++ utils/apiLogin.py
@@ -1,9 +1,6 @@
-import logging
 import requests
 import os
 from dotenv import load_dotenv
-
-log = logging.getLogger(__name__)
 
 
 class WebUntisClient:
@@ -12,28 +9,12 @@ class WebUntisClient:
         self.session = requests.Session()
 
         self.token: str | None = None
-        self.user: dict | None = None
+        self.jsessionid: str | None = None
 
 
-    def login(self, username: str, password: str) -> dict:
-        log.info(
-            "Logging in as %s in school %s on %s.webuntis.com",
-            username,
-
-        )
-
+    def login(self, username: str, password: str) -> None:
         self._fetch_cookies(username, password)
         self._fetch_token()
-
-        self.user = self._fetch_user()
-
-        log.info(
-            "Logged in as %s (%s)",
-            self.user["displayName"],
-            self.user["username"],
-        )
-
-        return self.user
 
 
     def _fetch_cookies(self, username: str, password: str):
@@ -53,13 +34,13 @@ class WebUntisClient:
 
         if response.status_code == 404:
             raise RuntimeError("School or server not found.")
-
         response.raise_for_status()
 
         if not self.session.cookies:
             raise RuntimeError("No cookies received.")
-
-        log.info("Received %d cookies.", len(self.session.cookies))
+        
+        self.jsessionid = self.session.cookies.get("JSESSIONID")
+        return self.session.cookies.get_dict()
 
 
     def _fetch_token(self):
@@ -69,7 +50,6 @@ class WebUntisClient:
         )
 
         response.raise_for_status()
-
         token = response.text.strip()
 
         if "loginError" in token:
@@ -79,41 +59,16 @@ class WebUntisClient:
             raise RuntimeError("No bearer token received.")
 
         self.token = token
+        return self.token
 
-        log.info("Received bearer token.")
-
-
-    def _fetch_user(self) -> dict:
-        response = self.session.get(
-            f"{self.base_url}/api/rest/view/v1/app/data",
-            headers={
-                "Authorization": f"Bearer {self.token}",
-            },
-            timeout=15,
-        )
-
-        response.raise_for_status()
-
-        data = response.json()
-
-        if "user" not in data:
-            raise RuntimeError("No user data returned.")
-
-        user = data["user"]
-
-        return {
-            "id": user["person"]["id"],
-            "username": user["name"],
-            "displayName": user["person"]["displayName"],
-            "imageUrl": user["person"].get("imageUrl"),
-        }
 
 
 if __name__ == "__main__":
     load_dotenv()
 
     client = WebUntisClient(base_url=os.getenv("BASE_URL"))
-    user = client.login(username=os.getenv("UNTIS_USERNAME"), password=os.getenv("UNTIS_PASSWORD"),)
+    client.login(username=os.getenv("UNTIS_USERNAME"), password=os.getenv("UNTIS_PASSWORD"),)
 
-    print(user)
-    print(client.token)
+    print(f"{client.token = }")
+    print(f"{client.jsessionid = }")
+    print(f"{client.session.cookies.get_dict() = }")
