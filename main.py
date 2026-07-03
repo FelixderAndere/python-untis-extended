3aa27bf1ba4098811701fd046a5b5e7703b0e0f7
M <145050460+mrtncode@users.noreply.github.com>
2026-07-03 07:03:09 +0000

    Add more classes

--- main.py
+++ main.py
@@ -9,3 +9,5 @@ class Untis():
     def __init__(self) -> None:
         self.sesion = session.session()
         self.homework = homework.Homework(self.sesion)
+        self.timetable = timetable.Timetable(self.sesion)
+        self.schoolsearch = schoolsearch.SchoolSearch() # No session needed
---
+++ schoolsearch.py
@@ -0,0 +1,10 @@
+
+class SchoolSearch:
+    def __init__(self):
+        pass
+
+    def search_school(self, search_query: str):
+        """ Searches for schools using the Webuntis school search 
+        :param search_query: The search query string.
+        :return: A list of schools matching the search query."""
+        pass
